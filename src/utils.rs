35cb0bbbbef6f04346c47d790bae332b987cb129
Andrey <andreykaere@gmail.com>
2024-01-02 01:17:45 +0300

    Backup

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,74 @@
 use x11rb::connection::Connection;
-use x11rb::protocol::xproto::*;
+// use x11rb::protocol::xproto::*;
 use x11rb::protocol::Event;
 use x11rb::xcb_ffi::XCBConnection;
 
 use x11rb::connection::RequestConnection as _;
-use x11rb::protocol::xkb::{self, ConnectionExt as _};
-use x11rb::protocol::xproto::EventMask;
+use x11rb::protocol::xkb::{self, ConnectionExt as _, PerClientFlag};
+use x11rb::protocol::xproto::{
+    AtomEnum, ConnectionExt as _, EventMask, Grab, GrabMode, ModMask, Window,
+};
 use x11rb::wrapper::ConnectionExt as _;
 use xkbcommon::xkb as xkbc;
 
+use std::collections::HashSet;
+use std::process::Command;
 use std::sync::{self, Mutex};
 use std::thread;
 use std::time::Duration;
 
-fn is_window_valid<Conn: Connection>(conn: &Conn, window: Window) -> bool {
-    match conn.get_window_attributes(window).unwrap().reply() {
-        Ok(_) => true,
-        _ => false,
+use anyhow::bail;
+use mktemp::Temp;
+
+#[derive(Default)]
+pub struct Mods {
+    shift: bool,
+    alt: bool,
+    ctrl: bool,
+    super_key: bool,
+}
+
+pub struct Binding {
+    letter: char,
+    mods: Mods,
+}
+
+impl Binding {
+    fn from_keys(input: &[String]) -> anyhow::Result<Self> {
+        let mut mods = Mods::default();
+        let mut keys = input.to_vec();
+
+        if keys.contains(&"Shift".to_string()) {
+            keys.retain(|x| x.as_str() != "Shift");
+            mods.shift = true;
+        }
+
+        if keys.contains(&"Alt".to_string()) {
+            keys.retain(|x| x.as_str() != "Alt");
+            mods.alt = true;
+        }
+
+        if keys.contains(&"Ctrl".to_string()) {
+            keys.retain(|x| x.as_str() != "Ctrl");
+            mods.ctrl = true;
+        }
+
+        if keys.contains(&"Super".to_string()) {
+            keys.retain(|x| x.as_str() != "Super");
+            mods.super_key = true;
+        }
+
+        if keys.len() > 1 {
+            bail!("Incorrect input");
+        } else {
+            let letter = keys[0].chars().next().unwrap();
+            Ok(Self { letter, mods })
+        }
     }
+}
+
+fn is_window_valid<Conn: Connection>(conn: &Conn, window: Window) -> bool {
+    conn.get_window_attributes(window).unwrap().reply().is_ok()
 }
 
 fn get_wm_name<Conn: Connection>(conn: &Conn, window: Window) -> anyhow::Result<String> {
@@ -88,27 +139,81 @@ pub fn get_inkscape_ids<Conn: Connection>(conn: &Conn) -> anyhow::Result<Vec<Win
     Ok(inkscape_ids)
 }
 
-pub fn grab_keyboard<Conn: Connection>(conn: &Conn, window: Window) {
-    conn.grab_key(
-        false,
-        window,
-        ModMask::ANY,
-        Grab::ANY,
-        GrabMode::ASYNC,
-        GrabMode::ASYNC,
-    )
-    .unwrap();
+// pub fn grab_keyboard<Conn: Connection>(conn: &Conn, window: Window) {
+//     conn.grab_key(
+//         false,
+//         window,
+//         ModMask::ANY,
+//         Grab::ANY,
+//         GrabMode::ASYNC,
+//         GrabMode::ASYNC,
+//     )
+//     .unwrap();
+// }
+
+// pub fn key_to_char(key: u8, state: &xkbc::State) -> String {
+//     let sym = state.key_get_consumed_mods(key.into());
+
+//     println!("sym: {:?}", sym);
+
+//     state.key_get_utf8(key.into())
+// }
+
+pub fn parse_combination(string: &str) -> HashSet<String> {
+    let mut comb = HashSet::new();
+
+    for letter in string.split('+') {
+        comb.insert(letter.to_string());
+    }
+
+    comb
+    // HashSet::from(string.split('+').map(|x| x.to_string()).collect::Vec<_>().into())
 }
 
-pub fn key_to_char(key: u8, state: &xkbc::State) -> String {
-    let sym = state.key_get_consumed_mods(key.into());
-
-    println!("sym: {:?}", sym);
-
-    state.key_get_utf8(key.into())
+pub fn open_editor(filename: &str) {
+    Command::new("alacritty")
+        .args([
+            "msg",
+            "create-window",
+            "--class",
+            "popup-bottom-center,scratchpad",
+            "-e",
+            "vim",
+            "-u",
+            "~/.minimal-tex-vimrc",
+            // filename,
+        ])
+        .spawn();
+    // Command::new("urxvt")
+    //     .args([
+    //         "-name",
+    //         "popup-bottom-center",
+    //         "-e",
+    //         "vim",
+    //         "-u",
+    //         "~/.minimal-tex-vimrc",
+    //         filename,
+    //     ])
+    //     .spawn();
+    // subprocess.run([
+    //     'urxvt',
+    //     '-name', 'popup-bottom-center',
+    //     '-e', "vim",
+    //     "-u", "~/.minimal-tex-vimrc",
+    //     f"{filename}",
+    // ])
 }
 
-pub fn execute_command(command: &str) -> anyhow::Result<()> {
+pub fn execute_command(combination: &HashSet<String>) -> anyhow::Result<()> {
+    if combination == &parse_combination("t") {
+        println!("hello!");
+        let file = Temp::new_file()?.to_path_buf();
+        let filename = file.to_str().unwrap();
+        open_editor(&filename);
+    }
+    if combination == &parse_combination("a+s") {
+        todo!();
+    }
     // todo!();
     //
 
@@ -120,6 +225,7 @@ pub fn filter_key<Conn: Connection>(
     window: Window,
     event: Event,
     state: &xkbc::State,
+    buffer: &mut HashSet<String>,
 ) -> anyhow::Result<()> {
     let key = match event {
         Event::KeyPress(e) | Event::KeyRelease(e) => e.detail,
@@ -129,20 +235,33 @@ pub fn filter_key<Conn: Connection>(
     };
 
     // let letter: &str = &key_to_char(key, state);
-    let letter = key_to_char(key, state);
+    // let letter = key_to_char(key, state);
+    let letter = state.key_get_utf8(key.into());
 
     println!("{}, {}", key, letter);
 
     if let Event::KeyPress(e) = event {
-        if execute_command(&letter).is_err() {
-            println!("I pressed {letter}");
-            conn.send_event(true, window, EventMask::KEY_PRESS, e)?;
-        }
+        println!("state: {:?}", e.state);
+        // let foo: u32 = xproto::ModMask::SHIFT.into();
+        // println!("{:?}", foo != 0);
+        // println!("{:?}", xproto::ModMask::CONTROL as u32 != 0);
+        // if execute_command(&letter).is_err() {
+        println!("I pressed {letter}");
+
+        // conn.send_event(false, window, EventMask::NO_EVENT, e)?;
+
+        buffer.insert(letter.clone());
     }
 
     if let Event::KeyRelease(e) = event {
         println!("I released {letter}");
-        conn.send_event(true, window, EventMask::KEY_RELEASE, e)?;
+        // conn.send_event(false, window, EventMask::NO_EVENT, e)?;
+
+        // buffer.remove(&letter);
+        println!("Buffer is: {:?}", buffer);
+        execute_command(buffer);
+
+        buffer.clear();
     }
 
     conn.flush()?;
@@ -178,8 +297,28 @@ pub fn handle_inkscape_window(
     );
     let state = xkbc::x11::state_new_from_device(&keymap, &xcb_conn, device_id);
 
-    grab_keyboard(&conn, inkscape_window);
+    conn.xkb_per_client_flags(
+        device_id as u16,
+        PerClientFlag::DETECTABLE_AUTO_REPEAT,
+        PerClientFlag::DETECTABLE_AUTO_REPEAT,
+        Default::default(),
+        Default::default(),
+        Default::default(),
+    )?;
+
+    conn.grab_key(
+        false,
+        inkscape_window,
+        ModMask::ANY,
+        Grab::ANY,
+        GrabMode::ASYNC,
+        GrabMode::ASYNC,
+    )?;
     conn.flush()?;
+
+    // Buffer for pressed keys. Basically this is the combination of the keys,
+    // untill the release happens
+    let mut buffer = HashSet::new();
 
     while is_window_valid(&conn, inkscape_window) {
         // println!("foo {inkscape_window}");
@@ -193,7 +332,7 @@ pub fn handle_inkscape_window(
 
         match event {
             Event::KeyPress(_) | Event::KeyRelease(_) => {
-                filter_key(&conn, inkscape_window, event, &state)?;
+                filter_key(&conn, inkscape_window, event, &state, &mut buffer)?;
             }
 
             _ => {}
